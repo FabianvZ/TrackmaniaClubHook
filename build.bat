3c249977035e8ec96d88cd58c34e4c8e6d8c3902
FabianvZ <fnavanzetten@gmail.com>
2024-10-08 01:15:37 +0200

    Added license to build script

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 SET zip="C:\Program Files\7-Zip\7z.exe"
-SET name="DiscordRivalryPing.op"
+SET name="DiscordRivalryPing.zip"
 IF EXIST %name% DEL /F %name%
-%zip% a -mx9 -tzip %name% info.toml src
+%zip% a -mx9 -tzip %name% info.toml src LICENSE
