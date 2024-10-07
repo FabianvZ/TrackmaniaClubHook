7cfef5b7942fe79280c490a30d6f1120ec29ce40
FabianvZ <fnavanzetten@gmail.com>
2024-10-08 01:16:20 +0200

    Fixed build extension

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 SET zip="C:\Program Files\7-Zip\7z.exe"
-SET name="DiscordRivalryPing.zip"
+SET name="DiscordRivalryPing.op"
 IF EXIST %name% DEL /F %name%
 %zip% a -mx9 -tzip %name% info.toml src LICENSE
